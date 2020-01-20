e2a0c54d073ec7a8b526f0d4c8417ff20baa4995
ваше имяArtem <volkovartemka07@gmail.com>
2020-01-20 23:03:49 +0300

    Доделал задание

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 'use strict';
 // почему то скрипт работает только на первый блок, не понимаю почему
-var star = document.getElementById('star');
-var starActive = document.getElementById('star-active');
-
+var star = document.querySelector('.star');
+var starActive = document.querySelector('.star-active');
 star.addEventListener('click', function() {
-    starActive.classList.add('block-active');
-    star.classList.add('star-no-active');
+	starActive.classList.add('block-active');
+	star.classList.add('star-no-active');
 });
 starActive.addEventListener('click', function() {
-    starActive.classList.remove('block-active');
-    star.classList.remove('star-no-active');
+	starActive.classList.remove('block-active');
+	star.classList.remove('star-no-active');
 });
